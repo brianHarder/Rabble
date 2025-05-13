d6bc95ac0ac119aadea4634d0fbacee3e432590f
Brian Harder <briankharder@gmail.com>
2025-05-13 12:00:09 -0500

    fixture update

--- src/rabble/tests/factories.py
+++ src/rabble/tests/factories.py
@@ -1,4 +1,4 @@
-from factory import Sequence, Faker, SubFactory, PostGenerationMethodCall
+from factory import Sequence, Faker, SubFactory, PostGenerationMethodCall, post_generation
 from factory.django import DjangoModelFactory
 from rabble.models import User, Rabble, SubRabble, Post, Comment
 
@@ -15,14 +15,12 @@ class UserFactory(DjangoModelFactory):
     date_joined = Faker("date_time_this_year", before_now=True, after_now=True)
     password = PostGenerationMethodCall("set_password", "password")
 
-
 class CommunityFactory(DjangoModelFactory):
     class Meta:
         model = Rabble
 
     community_id = Sequence(lambda n: f"community-{n}")
     owner = SubFactory(UserFactory)
-
 
 class SubRabbleFactory(DjangoModelFactory):
     class Meta:
@@ -32,10 +30,17 @@ class SubRabbleFactory(DjangoModelFactory):
     subrabble_name = Faker("sentence", nb_words=3)
     description = Faker("paragraph")
     allow_anonymous = True
-    private = Faker("boolean")
+    private = False
     rabble_id = SubFactory(CommunityFactory)
     user_id = SubFactory(UserFactory)
 
+    @post_generation
+    def members(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for user in extracted:
+                self.members.add(user)
 
 class PostFactory(DjangoModelFactory):
     class Meta:
@@ -46,7 +51,6 @@ class PostFactory(DjangoModelFactory):
     user_id = SubFactory(UserFactory)
     subrabble_id = SubFactory(SubRabbleFactory)
     anonymous = False
-
 
 class CommentFactory(DjangoModelFactory):
     class Meta:
