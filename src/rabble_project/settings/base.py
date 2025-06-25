58366d2f3f4f647ff749080c12f6bbca56689061
Brian Harder <briankharder@gmail.com>
2025-06-24 20:28:04 -0700

    CI fix

--- src/rabble_project/settings/base.py
+++ src/rabble_project/settings/base.py
@@ -111,10 +111,10 @@ MEDIA_URL = env('MEDIA_URL', default='https://rabble-bucket.s3.amazonaws.com/med
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
-AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
-AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default="test-bucket")
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default="test-access-key")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default="test-secret-key")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="us-west-1")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
