82542059592efefbf68c1f61d8ad4c04e77d3490
Ubuntu <ubuntu@ip-172-31-31-117.us-west-1.compute.internal>
2025-06-29 23:23:25 +0000

    Pytest settings fix

--- src/rabble_project/settings/production.py
+++ src/rabble_project/settings/production.py
@@ -7,7 +7,7 @@ DATABASES = {'default': env.db()}
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["rabbleapp.me"])
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["https://rabbleapp.me"])
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 STATICFILES_DIRS = [BASE_DIR / "static"]
